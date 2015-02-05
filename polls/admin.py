dfe22cde309e360c066d0b0ca37135ddb541d116
Daniel R <djr9478@rit.edu>
2015-02-05 12:10:42 -0500

    Finished part 2

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from polls.models import Poll
+from polls.models import Choice
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+    #fieldsets = [None, {'fields': ['Choices'], 'classes': ['collapse']}
 
 class PollAdmin(admin.ModelAdmin):
-    fields = ['pub_date','question']
+##    fieldsets = [
+##        (None,               {'fields': ['question']}),
+##        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+##    ]
+    list_display = ('question','pub_date','was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question']
+    inlines = [ChoiceInline]
 
 admin.site.register(Poll, PollAdmin)
+admin.site.register(Choice)
--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,5 @@
 import datetime
+from django.utils import timezone
 from django.db import models
 
 class Poll(models.Model):
@@ -7,7 +8,10 @@ class Poll(models.Model):
     
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-    
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
